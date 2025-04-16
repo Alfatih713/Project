d4a2e2cf5d0c9c2e421c3f7233fa0d03a77c7486
Alfatih713 <alfatihnugroho713@gmail.com>
2025-04-16 20:37:08 +0700

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+name = "Alfatih"
+age = 11
+
+print("Hello",name)
+print("Umur :",age)
+
+address = ("Rumah")
+
+print(f"Welcome! {name}, Usia : {age}, Alamat : {address}")
