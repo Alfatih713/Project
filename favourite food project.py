c9ffbcc60ea66088a6d9cd29d6477ca47ee4b0bc
Alfatih713 <alfatihnugroho713@gmail.com>
2025-05-23 20:38:28 +0700

    Add files via upload

---
+++ favourite food project.py
@@ -0,0 +1,11 @@
+print('My Favourite Food is...')
+food1 = (input('Favourite Food 1 : '))
+food2 = (input('Favourite Food 2 : '))
+food3 = (input('Favourite Food 3 : '))
+
+fav_food = []
+
+fav_food.append(food1)
+fav_food.append(food2)
+fav_food.append(food3)
+print(fav_food)
